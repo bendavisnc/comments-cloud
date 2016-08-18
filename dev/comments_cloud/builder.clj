9b801ed0a0deb10401f9d0ea2a88a32803404346
Ben Davis <bendavisnc@gmail.com>
2016-08-18 17:50:18 -0400

    added pmap

--- dev/comments_cloud/builder.clj
+++ dev/comments_cloud/builder.clj
@@ -16,92 +16,112 @@
 (def get-all-files
   "Returns a list of all of the files given under the configured target directory that match the configured target extension"
   (fn []
-    (let [
-        root-dir 
-          (file-seq
-            (io/file (config :parse-dir)))
-        all-files
-          (filter
-            (fn [f]
-              (=
-                (config :target-ext)
-                (FilenameUtils/getExtension
-                  (.getName f))))
-            root-dir)
-      ]
-      all-files)))
+    (do
+      (println "@ get-all-files")
+      (time
+        (let [
+            root-dir 
+              (file-seq
+                (io/file (config :parse-dir)))
+            all-files
+              (filter
+                (fn [f]
+                  (=
+                    (config :target-ext)
+                    (FilenameUtils/getExtension
+                      (.getName f))))
+                root-dir)
+          ]
+          all-files)))))
+
+(def testbs
+  (fn []
+    (count
+      (file-seq
+        (io/file (config :parse-dir))))))
 
 (def build-raw-word-data
   "This is where most of the heavy lifting occurs.
    Goes through all the given files and creates one big list of maps,
    each containing all of the comments found in said file, along with the file path."
   (fn []
-    (map
-      (fn [each-target-file]
-        {
-          :full-path
-            (get-relative-file-path each-target-file)
-          :comments-found
-            (comments-util/get-all-comments-from-file each-target-file)
-        })
-      (get-all-files))))
+    (do
+      (println "@ build-raw-word-data")
+      (time
+        (map
+          (fn [each-target-file]
+            {
+              :full-path
+                (get-relative-file-path each-target-file)
+              :comments-found
+                (comments-util/get-all-comments-from-file each-target-file)
+            })
+          (get-all-files))))))
 
 (def spit-raw-word-data
   (fn []
-    (spit-proper
-      "./generated/raw-word-data.edn"
-      (clojure.pprint/write
-        (build-raw-word-data)
-        :stream nil
-        ))))
+    (do
+      (println "@ spit-raw-word-data")
+      (time
+        (spit-proper
+          "./generated/raw-word-data.edn"
+          (clojure.pprint/write
+            (build-raw-word-data)
+            :stream nil
+          ))))))
 
 
 (def build-word-count
   (fn [raw-word-data]
-    (sort-by
-      (fn [datum]
-        (*
-          (datum :count)
-          -1))
-      ; :count
-      (filter 
-        (fn [datum]
-          (not
-            (some #(= (datum :word) %) (config :blacklist))))
-        (map 
-          (fn 
-            [[k, v]]
-            {
-              :word k
-              :found-in v
-              :count (count v)
-            })
-          (let [
-              counts (atom {})
-            ]
-            (do
-              (doall
-                (map
-                  (fn [datum]
-                    (do
-                      (let [
-                          comments (datum :comments-found)
-                        ]
-                        (doall
-                          (map
-                            (fn [each-word]
-                              (if
-                                (contains? @counts each-word)
-                                  ; (swap! counts assoc each-word (inc (@counts each-word)))
-                                  ; (swap! counts assoc each-word 1)))
-                                  (swap! counts assoc each-word (conj (@counts each-word) (datum :full-path)))
-                                  (swap! counts assoc each-word [(datum :full-path)])))
-                            (mapcat
-                              (fn [each-comment-in-file]
-                                (clojure.string/split each-comment-in-file #" "))
-                              comments))))))
-                  raw-word-data))
-              @counts)))))))
+    (do
+      (println "@ build-word-count")
+      (time
+        (sort-by
+          (fn [datum]
+            (*
+              (datum :count)
+              -1))
+          ; :count
+          (filter 
+            (fn [datum]
+              (not
+                (some #(= (datum :word) %) (config :blacklist))))
+            (pmap 
+              (fn 
+                [[k, v]]
+                {
+                  :word k
+                  :found-in v
+                  :count (count v)
+                })
+              (let [
+                  counts (atom {})
+                ]
+                (do
+                  (doall
+                    (pmap
+                      (fn [datum]
+                        (do
+                          (let [
+                              comments (datum :comments-found)
+                            ]
+                            (doall
+                              (pmap
+                                (fn [each-word]
+                                  (if
+                                    (contains? @counts each-word)
+                                      ; (swap! counts assoc each-word (inc (@counts each-word)))
+                                      ; (swap! counts assoc each-word 1)))
+                                      (swap! counts assoc each-word (conj (@counts each-word) (datum :full-path)))
+                                      (swap! counts assoc each-word [(datum :full-path)])))
+                                (mapcat
+                                ; (concat
+                                  ; (pmap
+                                    (fn [each-comment-in-file]
+                                      (clojure.string/split each-comment-in-file #" "))
+                                    comments))))))
+                      raw-word-data))
+                  @counts)))))))))
 
 
 (def spit-word-count-data
@@ -125,8 +145,6 @@
       (build-simple-word-count-list
         (build-word-count
           (build-raw-word-data))))))
-
-
 
 (def testt
   (fn []
