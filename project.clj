be69be80073b3d570ceca27c28afecb27c81a0ac
Ben Davis <bendavisnc@gmail.com>
2016-08-18 18:02:18 -0400

    added mem params

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :jvm-opts ["-Xmx8g"]
 
   :min-lein-version "2.6.1"
 
